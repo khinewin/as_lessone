beee20cd0dd9a6076d3228e0f6ed0d615b56cc00
KhineWin <khinewin123@gmail.com>
2025-07-19 10:05:44 +0630

    signup

--- project/post-register.php
+++ project/post-register.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+include("process.php");
 
 $name=$_POST['name'];
 $email=$_POST['email'];
@@ -7,18 +8,23 @@ $pass=$_POST['password'];
 $confirm_pass=$_POST['confirm_password'];
 
 if(!$name){
-    $_SESSION['error_msg']="The name filed is required.";
+    $_SESSION['error_msg']="The name field is required.";
     header("location: register.php");
 }elseif(!$email){
-    $_SESSION['error_msg']="The email filed is required.";
+    $_SESSION['error_msg']="The email field is required.";
     header("location: register.php");
 }elseif(!$pass){
- $_SESSION['error_msg']="The password filed is required.";
+ $_SESSION['error_msg']="The password field is required.";
     header("location: register.php");
 }elseif(!$confirm_pass){
- $_SESSION['error_msg']="The confirm password filed is required.";
+ $_SESSION['error_msg']="The confirm password field is required.";
     header("location: register.php");
 }else if($pass != $confirm_pass){
      $_SESSION['error_msg']="The password and confirm password must match.";
     header("location: register.php");
+}else{
+    
+    $p=new Process();
+    $p->register($name, $email, $pass);
+    
 }
--- project/process.php
+++ project/process.php
@@ -8,6 +8,21 @@ class Process{
         $conn=new Database();
         $this->db=$conn->connect();
    }
+
+   public function register($name, $email, $pass){
+        $query="insert into users (name, email, password) values (:name, :email, :password)";
+        $stmt=$this->db->prepare($query);
+        $pass_hash=password_hash($pass, PASSWORD_BCRYPT);
+        $stmt->execute([":name"=>$name, ":email"=>$email,":password"=>$pass_hash ]);
+        $_SESSION['success_msg']="The user account has been signup.";
+        header("location: register.php");
+   }
+
+   //md5, sha1, bcrypt
+
+
+
+
     public function create(){    
 
         $name="mgmg";
@@ -25,6 +40,3 @@ class Process{
         return $stmt->fetchAll();
     }
 }
-
-$p=new Process();
-var_dump($p->getUsers());
