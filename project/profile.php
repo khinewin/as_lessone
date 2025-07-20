8feee5f769828e0b1a8106469b0dda5cd3832b51
KhineWin <khinewin123@gmail.com>
2025-07-20 11:13:33 +0630

    update profile

--- project/profile.php
+++ project/profile.php
@@ -9,9 +9,21 @@
 </head>
 <body>
 <?php include("navbar.php") ?>
-
     <div class="container">
         <h3>Profile</h3>
+        <hr>
+        <ul class="list-group">
+            <li class="list-group-item">Name :  <?php echo $_SESSION['user_auth']['name'] ?></li>
+            <li class="list-group-item">Email :  <?php echo $_SESSION['user_auth']['email'] ?></li>
+            <li class="list-group-item">Member since :  
+                <?php 
+                    $created_at=$_SESSION['user_auth']['created_at'];
+                    echo date("d-m-Y h:i:s A", strtotime($created_at));
+                ?>
+            </li>
+
+
+        </ul>
     </div>
     
     <script src="bst/js/popper.min.js"></script>
