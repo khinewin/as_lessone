982d1268bf234038f5978a934613756d05e16c05
KhineWin <khinewin123@gmail.com>
2025-07-20 10:37:18 +0630

    logout

--- project/navbar.php
+++ project/navbar.php
@@ -18,7 +18,7 @@
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Profile</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
+            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
           </ul>
         </li>
          <?php
--- project/post-login.php
+++ project/post-login.php
@@ -7,10 +7,10 @@ $pass=$_POST['password'];
 
 if(!$email){
      $_SESSION['error_msg']="The email field is required.";
-    header("location: register.php");
+    header("location: login.php");
 }else if(!$pass){
      $_SESSION['error_msg']="The password field is required.";
-    header("location: register.php");
+    header("location: login.php");
 }else {
     $p=new Process();
     $p->login($email, $pass);
