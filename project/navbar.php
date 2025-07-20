71fb9550b54a1270ab41594b3e74f19add4b04ad
KhineWin <khinewin123@gmail.com>
2025-07-20 09:48:36 +0630

    navbar

--- project/navbar.php
+++ project/navbar.php
@@ -1,3 +1,4 @@
+<?php session_start();   ?>
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container">
     <a class="navbar-brand" href="#">Project Name</a>
@@ -6,21 +7,30 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-       
+       <?php 
+        if(isset($_SESSION['user_auth'])):
+          $name=$_SESSION['user_auth']['name'];
+          ?>                
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            <?php echo $name; ?>
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#]">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
           </ul>
         </li>
-        <!--<li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>-->
+         <?php
+         else:
+          ?>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Login</a>
+        </li>
+          <?php
+        endif;
+       ?>
+        
       </ul>
       <!--<form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
